ac972f95086de23bd85c35de37da7281f76671f1
Demener <dnowakuf@gmail.com>
2014-07-15 16:21:57 -0400

    Re index setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-	:address		=> "smtp.gmail.com"
-	:port
+	:address				=> "smtp.gmail.com",
+	:port					=> 587,
+	:domain					=> "gmail.com",
+	:user_name				=> "gameglamors",
+	:password				=> "REDACTED!",
+	:authentication 		=> "plain",
+	:enable_starttls_auto	=> true
 }
